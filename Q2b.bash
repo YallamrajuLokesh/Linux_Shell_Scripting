d4354d36cd05b3256f86c104e74b03459bcc0be0
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-12-18 11:49:23 +0530

    Update Q2b.bash

--- Q2b.bash
+++ Q2b.bash
@@ -2,12 +2,13 @@
 # &amp; display its creation time and permissions of the file, on the standard
 # output
 
-! /bin/bash
+#! /bin/bash
 if [ $# -eq 0 ]
 then
 echo &quot;display does not exit&quot;
 else
-ls -l $1&gt; t1
-x=`cut -d &#39; &#39; -f 1,6,7,8,9 t1`
+ls -l $1> t1
+x=`cut -d ' ' -f 1,6,7,8,9 t1`
 echo $x
 fi
+
